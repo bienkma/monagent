4122671614536e3d8343cc0365a7f3643be8671b
bientd <bientd@gmail.com>
2016-04-26 15:46:34 +0700

    create module

--- collections/monitor.go
+++ collections/monitor.go
@@ -10,22 +10,42 @@ import (
 )
 
 // Function get information interfaces server
-func Network(Interfaces string) net.IOCountersStat {
+func Network(Interfaces string) (InfoNet net.IOCountersStat) {
 	v, _ := net.IOCounters(true)
-	var InfoNet net.IOCountersStat
 	// almost every return value is a struct
 	for i := 0; i < len(v); i++ {
 		if v[i].Name == Interfaces {
 			InfoNet = v[i]
 		}
 	}
-	return InfoNet
+	return
+}
+
+func Bandwidth(Interfaces string) (rx, tx float64) {
+	// Check rx, tx is fist time
+	t1 := time.Now()
+	last_rx := Network(Interfaces).BytesRecv
+	last_tx := Network(Interfaces).BytesSent
+
+	// check rx, tx is second time
+	time.Sleep(1 * time.Second)
+	t2 := time.Now()
+	now_rx := Network(Interfaces).BytesRecv
+	now_tx := Network(Interfaces).BytesSent
+
+	// rx, tx = (now_rx - last_rx) * 8 / delta_time(t2-t1)
+	delta_time := t2.Sub(t1).Nanoseconds()
+	rx = float64(now_rx-last_rx) * 8 / float64(delta_time) / 1000000000
+	tx = float64(now_tx-last_tx) * 8 / float64(delta_time) / 1000000000
+
+	return
+
 }
 
 // Function get information Memory
 func Memory() (uint64, uint64, uint64) {
 	InfoMem, _ := mem.VirtualMemory()
-	return InfoMem.Used, InfoMem.Cached, InfoMem.Total
+	return InfoMem.Total, InfoMem.Used, InfoMem.Cached
 }
 
 func CPU() []float64 {
--- mon.go
+++ mon.go
@@ -6,6 +6,7 @@ import (
 	"github.com/bienkma/monagent/collections"
 	"github.com/olivere/elastic"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -14,75 +15,81 @@ type Configuration struct {
 	HostName    string
 	Address     string
 	Interface   string
-	Application string // Example orm, antispam, bigdata
+	Application string
 	Location    string
 	ElasticURL  []string
 }
 
 // Define struct for doc in elasticsearch
 type host struct {
-	HostName      string    `json: "hostname"`
-	Address       string    `json: "address"`
-	TimeStamp     time.Time `json: "timestamp"`
-	Location      string    `json: "location"`
-	CpuPercent    float64   `json: "cpu_usage"`
-	MemoryTotal   float64   `json: "memory_total"`
-	MemoryUsage   float64   `json: "memory_usage"`
-	MemoryCache   float64   `json: "memory_usage"`
-	BandWidthRec  float64   `json: "net_in"`
-	BandwidthSent float64   `json: "net_out"`
+	HostName      string  `json: "hostname"`
+	Address       string  `json: "address"`
+	Timestamp     string  `json: "timestamp"`
+	Location      string  `json: "location"`
+	CpuPercent    float64 `json: "cpu_usage"`
+	MemoryTotal   uint64  `json: "memory_total"`
+	MemoryUsage   uint64  `json: "memory_usage"`
+	MemoryCache   uint64  `json: "memory_usage"`
+	BandWidthRec  float64 `json: "net_in"`
+	BandwidthSent float64 `json: "net_out"`
 }
 
 // The main function
 func main() {
+	for {
+		// Define configuration file
+		file, _ := os.Open("config.json")
+		decoder := json.NewDecoder(file)
+		configuration := Configuration{}
 
-	// Define configuration file
-	file, _ := os.Open("conf.json")
-	decoder := json.NewDecoder(file)
-	configuration := Configuration{}
+		err := decoder.Decode(&configuration)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		// close define Configuration
 
-	err := decoder.Decode(&configuration)
-	if err != nil {
-		fmt.Println("Error:", err)
+
+		t := time.Now().Format("2006-01-02T15:04:05.999999")
+
+		mem_total, mem_usage, mem_cache := collections.Memory()
+		rx, tx := collections.Bandwidth(configuration.Interface)
+
+		doc := host{
+			HostName:      configuration.HostName,
+			Address:       configuration.Address,
+			Timestamp:     t,
+			Location:      configuration.Location,
+			CpuPercent:    collections.CPU()[0],
+			MemoryTotal:   mem_total,
+			MemoryUsage:   mem_usage,
+			MemoryCache:   mem_cache,
+			BandWidthRec:  rx,
+			BandwidthSent: tx,
+		}
+		fmt.Printf("doc:%v ", doc)
+		// Create connection
+		client, err := elastic.NewClient(
+			elastic.SetURL(configuration.ElasticURL[0]),
+		)
+
+		if err != nil {
+			fmt.Printf("Connection fail!...")
+		}
+
+		// Add document to the index
+		t2 := time.Now()
+		_, err = client.Index().
+			Index(configuration.Application).
+			Type(configuration.HostName).
+			Id(strconv.FormatInt(t2.UnixNano(), 10)).
+			BodyJson(doc).
+			Do()
+		if err != nil {
+			fmt.Printf("Error create index")
+			panic(err)
+		}
+
+		fmt.Printf("Time: %v \n", strconv.FormatInt(t2.UnixNano(), 10))
+
 	}
-	// close define Configuration
-
-	t := time.Now().UTC()
-
-	doc := host{
-		HostName:      configuration.HostName,
-		Address:       configuration.Address,
-		TimeStamp:     t,
-		Location:      configuration.Location,
-		CpuPercent:    collections.CPU(),
-		MemoryTotal:   collections.Memory(),
-		MemoryUsage:   512,
-		MemoryCache:   256,
-		BandWidthRec:  10,
-		BandwidthSent: 20,
-	}
-
-	// Create connection
-	client, err := elastic.NewClient(
-		elastic.SetURL(configuration.ElasticURL[0]),
-	)
-
-	if err != nil {
-		fmt.Printf("Connection fail!...")
-	}
-
-	// Add document to the index
-	_, err = client.Index().
-		Index(configuration.Application).
-		Type(configuration.HostName).
-		Id(strconv.FormatInt(t.UnixNano(), 10)).
-		BodyJson(doc).
-		Do()
-	if err != nil {
-		fmt.Printf("Error create index")
-		panic(err)
-	}
-
-	fmt.Printf("Time: %v \n", strconv.FormatInt(t.UnixNano(), 10))
-
 }
